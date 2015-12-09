da99a23f7b20d95407d79237f3ee083fd6318bbb
tamick88 <tamick88@gmail.com>
2015-12-09 15:10:40 -0700

    Adding the rest of the ex1 files

---
+++ ex1/ex1ec.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	int langauges = 3;
+	
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+	printf("Hi, Zed Shaw!\n");
+	printf("I'm using this book to learn C programming.\n");
+	printf("After this I will know %d languages.\n", langauges);
+	printf("Wish me luck!\n");
+	
+	return 0;
+}
